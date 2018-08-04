dbf65ec0fc16f2db4a006d0a2976d050b9c38daf
liuliaixue <liuliaixue@qq.com>
2018-08-04 10:04:04 +0800

    add test

---
+++ test/index.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const app = express();
+const responseTime = require('../index')
+
+
+const timeout = ms => new Promise(r => setTimeout(() => r(), ms))
+
+app.use(responseTime((methond, url, time) => {
+    console.log(`${methond} ${url} ${time}ms`)
+}))
+app.get('/time', async (req, res, next) => {
+    await timeout(1000)
+    res.end('response after 1000ms ')
+})
+
+app.listen(process.env.PORT || 8080, () => {
+    console.log(`server is listenning on ${process.env.PORT || 8080}`)
+})
